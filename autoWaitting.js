4494e90f4a041896b353035fb2f77da7c0ccfdcb
codethoisinhvien <nguyentruonggiang28081999@gmail.com>
2018-11-27 08:42:03 +0700

    aaaa

--- autoWaitting.js
+++ autoWaitting.js
@@ -8,133 +8,154 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-let test = ["INT2209 7","INT2208 5","INT3110 3","INT2206 2","PHI1005 3","HIS1002 15","INT3207 1","PES1020 36  TH","PES1017 35 TH"];
-let testbeak=["MAT1042 7","HIS1002 14","INT2208 7"];
-$registrationAvailable=true;
-let reSuject=[];
-let breakSuject=[]
-let sum =undefined;
-function searchBreak(name){
-	
-	let rows = document.getElementsByClassName('table table-hover table-bordered')[1];
-	for(let i=1;i<rows.rows.length;i++){
-		if(rows.rows[i].cells[3].innerText.trim()==name){
-		let r1=rows.rows[i].cells[9].getElementsByTagName("a")[0].getAttribute("data-rowindex");
-		console.log(r1)
-		breakSuject.push(r1);
-		}
-	}
-	
-}
-function fakeAbort(n)
-{
-    var r = (new Date).getTime(), i = "", t = "/huy-mon-hoc/" + n + "/" + $registrationMode + "/" + $dsdkMod;
-    return $.ajax({ type: "POST", cache: !1, async: !1, url: t, dataType: "json", success: function (n)
-    {
-        i = n.message;
-        sendPageViewData(t)
-    }, complete: function ()
-    {
-        var n = (new Date).getTime(), i = n - r;
-        sendUserTimeCallingAjax(t, i);
-        sendEventCallingAjax(t)
-    }
-                  }), i
-}
-function breakFull(array){
-	for(let i=0;i<array.length;i++){
-		searchBreak(array[i]);
-	}
-	breakSuject.forEach(function(g){
-		fakeAbort(g)
-	})
-}
+    let test = [{sb:"INT2207 4 (TH)",time:"T3-(1-2)-PM 201-G2"}];
+    let testbeak = ["MAT1042 7", "HIS1002 14", "INT2208 7"];
+    $registrationAvailable = true;
+    let reSuject = [];
+    let breakSuject = []
+    let sum = undefined;
+    function searchBreak(name) {
 
-
-function searchSubject(name){
-    name=name.trim();
-    var rows=  document.getElementsByClassName('table table-hover table-bordered')[0];
-
-    for(let i=0;i<rows.rows.length;i++){
-
-     if(rows.rows[i].cells[4].innerText.trim()==name){
-
-        if(rows.rows[i].cells[0].getElementsByTagName("input").length>0){
-
-        let r1=rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-rowindex");
-        let r2=rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-numcrd");
-        let No = parseInt(rows.rows[i].cells[2].innerText.trim());
-
-        sum+=1;
-        console.log(r1)
-      reSuject.push({name:name,r:r1,u:r2})
-
-        }
-    }
-
-}
-}
-function fakeRegistration(sum){
-    var t = "/xac-nhan-dang-ky/" + $registrationMode;
-        ajaxRequest("POST", t, "json", sum, null, null, !0);
-}
-function addSubject(array){
-    reSuject=[]
-for(let i=0;i<array.length;i++){
-    searchSubject(array[i]);
-
-
-}
-     reSuject.forEach(function(g){
-        fakePending(g.r);
-    })
-
-}
-
-function fakePending(n)
-{
-    var r = (new Date).getTime(), i = "", t = "/chon-mon-hoc/" + n + "/" + $registrationMode + "/" + $dsdkMod;
-    return $.ajax({ type: "POST", cache: !1, async: !1, url: t, dataType: "json", success: function (n)
-    {
-        i = n.message;
-
-        sendPageViewData(t)
-    }, complete: function ()
-    {
-        var n = (new Date).getTime(), i = n - r;
-        sendUserTimeCallingAjax(t, i);
-        sendEventCallingAjax(t)
-		console.log("complete 1" + n);
-    }
-                  })
-
-}
-
-var main = function(){
-	if(sum!=undefined){
-        var sum2=sum;
-           
-        addSubject(test);
-        if(sum2<sum){
-		fakeRegistration(sum);
+        let rows = document.getElementsByClassName('table table-hover table-bordered')[1];
+        for (let i = 1; i < rows.rows.length; i++) {
+            if (rows.rows[i].cells[3].innerText.trim() == name) {
+                let r1 = rows.rows[i].cells[9].getElementsByTagName("a")[0].getAttribute("data-rowindex");
+                console.log(r1)
+                breakSuject.push(r1);
+            }
         }
 
-        sum=undefined;
-    }else{
-        DSDK(2);
-		breakFull(testbeak);
-		sum = parseInt($('.total-credit-container').text());
-	}
-	console.log("running")
+    }
+    function fakeAbort(n) {
+        var r = (new Date).getTime(), i = "", t = "/huy-mon-hoc/" + n + "/" + $registrationMode + "/" + $dsdkMod;
+        return $.ajax({
+            type: "POST", cache: !1, async: !1, url: t, dataType: "json", success: function (n) {
+                i = n.message;
+                sendPageViewData(t)
+            }, complete: function () {
+                var n = (new Date).getTime(), i = n - r;
+                sendUserTimeCallingAjax(t, i);
+                sendEventCallingAjax(t)
+            }
+        }), i
+    }
+    function breakFull(array) {
+        for (let i = 0; i < array.length; i++) {
+            searchBreak(array[i]);
+        }
+        breakSuject.forEach(function (g) {
+            fakeAbort(g)
+        })
+    }
 
-}
-$(document).ready(function() {
-	setInterval(function(){
-		window.location.reload()
-	},150000)
-	setInterval(main,2000);
-})
-})();
+
+    function searchSubject(name) {
+        name.sb = name.sb.trim();
+        var rows = document.getElementsByClassName('table table-hover table-bordered')[0];
+
+        for (let i = 0; i < rows.rows.length; i++) {
+
+            if (rows.rows[i].cells[4].innerText.trim() == name.sb && name.time === 0) {
+
+                if (rows.rows[i].cells[0].getElementsByTagName("input").length > 0) {
+
+                    let r1 = rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-rowindex");
+                    let r2 = rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-numcrd");
+                    let No = parseInt(rows.rows[i].cells[2].innerText.trim());
+
+                    sum += 1;
+                    console.log(r1)
+                    reSuject.push({ name: name, r: r1, u: r2 })
+
+                }
+            }
+                if (rows.rows[i].cells[4].innerText.trim() == name.sb && name.time != 0) {
+                    console.log(rows.rows[i].cells[9].innerText.trim())
+                    if (rows.rows[i].cells[9].innerText.trim() == name.time.trim()) {
+                        if (rows.rows[i].cells[0].getElementsByTagName("input").length > 0) {
+
+                            let r1 = rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-rowindex");
+                            let r2 = rows.rows[i].cells[0].getElementsByTagName("input")[0].getAttribute("data-numcrd");
+                            let No = parseInt(rows.rows[i].cells[2].innerText.trim());
+
+                            sum += 1;
+                            console.log(r1)
+                            reSuject.push({ name: name, r: r1, u: r2 })
+
+                        }
+
+                    }
+                }
+
+            }
+
+    }
+        function fakeRegistration(sum) {
+            var t = "/xac-nhan-dang-ky/" + $registrationMode;
+            ajaxRequest("POST", t, "json", sum, null, null, !0);
+        }
+        function addSubject(array) {
+            reSuject = [];
+            for (let i = 0; i < array.length; i++) {
+
+                searchSubject(array[i]);
+
+
+            }
+            reSuject.forEach(function (g) {
+                for (let i = 0; i < test.length; i++) {
+                    if (test[i] == g.name) {
+                        test.splice(i, 1)
+                    }
+                }
+                fakePending(g.r);
+            })
+
+        }
+
+        function fakePending(n) {
+            var r = (new Date).getTime(), i = "", t = "/chon-mon-hoc/" + n + "/" + $registrationMode + "/" + $dsdkMod;
+            return $.ajax({
+                type: "POST", cache: !1, async: !1, url: t, dataType: "json", success: function (n) {
+                    i = n.message;
+
+                    sendPageViewData(t)
+                }, complete: function () {
+                    var n = (new Date).getTime(), i = n - r;
+                    sendUserTimeCallingAjax(t, i);
+                    sendEventCallingAjax(t)
+                    console.log("complete 1" + n);
+                }
+            })
+
+        }
+
+        var main = function () {
+            if (sum != undefined) {
+                var sum2 = sum;
+
+                addSubject(test);
+                if (sum2 < sum) {
+                    fakeRegistration(sum);
+                }
+
+                sum = undefined;
+            } else {
+
+                breakFull(testbeak);
+                DSDK(2);
+                sum = parseInt($('.total-credit-container').text());
+            }
+            console.log("running")
+
+        }
+        $(document).ready(function () {
+            setInterval(function () {
+                window.location.reload()
+            }, 150000)
+            setInterval(main, 2000);
+        })
+    }) ();
